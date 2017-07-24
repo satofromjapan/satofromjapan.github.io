e4abef0f8c5ef6a3da9d2be6f30bd582ad7c1681
Masato Takahashi <masato.takaha@gmail.com>
2017-07-24 13:10:19 -0700

    Added in 'About' text, fixed some of the text colors, and set navbar to fixed

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
   $('.parallax').parallax();
   $('.materialboxed').materialbox();
+  $("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
 });
